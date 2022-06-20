896bafe375594f7f8e75c46bc6b572a711f95704
Maksim Belokon <krasnotura26@yandex.ru>
2022-06-20 19:33:25 +0300

    5pr 1rev

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,6 @@
 const popupProfile = document.querySelector('.popup-profile');
 const popupCard = document.querySelector('.popup-card');
 const popupPic = document.querySelector('.popup-picture');
-const popup = document.querySelector('.popup');
 
 //объявил название месста и ссылки на изображение в попапе с картинкой
 const popupPicText =  document.querySelector('.popup-picture__text');
@@ -27,7 +26,7 @@ const cardTemplate = document.querySelector('#card').content;
 
 // объявил кнопки редактирования, добавления карточки
 const profileEditButton = document.querySelector('.profile__edit-button');
-const CardAddButton = document.querySelector('.profile__add-button');
+const cardAddButton = document.querySelector('.profile__add-button');
 
 // объявил кнопки закрытия попапов 
 const popupProfileCloseButton = document.querySelector('.popup-profile__close-button');
@@ -38,85 +37,94 @@ const popupPicCloseButton = document.querySelector('.popup-picture__close-button
 const formElementCard = document.querySelector('#form-card');
 const formElementProfile = document.querySelector('#form-profile');
 
-// функциии открытия всех попапов
-function popupProfileShow () {
-    popupProfile.classList.remove('popup_hidden');
+// функциии открытия попапов
+function openPopup (popup) {
+  popup.classList.add('popup_opened');
+}  
+
+function showPopupProfile () {
+    openPopup (popupProfile);
     inputSubtitle.value = profileSubtitle.textContent;
     inputTitle.value = profileTitle.textContent;
 }
-profileEditButton.addEventListener('click', popupProfileShow);
+profileEditButton.addEventListener('click', showPopupProfile);
 
-function popupCardShow () {
-    popupCard.classList.remove('popup_hidden'); 
-}
-CardAddButton.addEventListener('click', popupCardShow);
-
-function popupPicShow () {
-    popupPic.classList.remove('popup_hidden');
-};
+cardAddButton.addEventListener('click', function () {
+  openPopup(popupCard)
+});
 
 //функции закрытия всех попапов с обработчиками кликов по ним
-function profileClose () {
-    popupProfile.classList.add('popup_hidden');
+function closePopup (popup) {
+    popup.classList.remove('popup_opened');
 };
-function addCardClose () {
-  popupCard.classList.add('popup_hidden');
-};
-function picClose () {
-  popupPic.classList.add('popup_hidden');
-};
-popupCardCloseButton.addEventListener('click', addCardClose);
-popupProfileCloseButton.addEventListener('click', profileClose);
-popupPicCloseButton.addEventListener('click', picClose);
+popupCardCloseButton.addEventListener('click', function (){
+  closePopup(popupCard);
+});
+popupProfileCloseButton.addEventListener('click', function (){
+  closePopup(popupProfile);
+});
+popupPicCloseButton.addEventListener('click', function (){
+  closePopup(popupPic);
+});
 
 // объявил форму профиля, повесил на кнопку сабмита изменение имени и работы, и закрытие попапа при сабмите
-function formSubmitHandler (evt) {
+function handleProfileFormSubmit (evt) {
     evt.preventDefault();
     profileTitle.textContent = inputTitle.value;
     profileSubtitle.textContent = inputSubtitle.value;
-    profileClose ();
+    closePopup(popupProfile);
 };
-formElementProfile.addEventListener('submit', formSubmitHandler); 
+formElementProfile.addEventListener('submit', handleProfileFormSubmit); 
 
 //функция открытия попапа картинки
 function addPopupPic (item) {
+  openPopup(popupPic);
   popupPicImage.src = item.link;
-  popupPicText.innerText = item.name;
-  popupPicShow ();
+  popupPicText.textContent = item.name;
 };
 
+//функция вставления карточки в начало
+function addInBegin (card) {
+  listElements.prepend(card);
+}
+//функция добавления лайка
+function addLike (evt) {
+  evt.target.classList.toggle('element__like_active');
+}
+//функция удаления карточки
+function deleteCard (evt) {
+  evt.target.closest('.element').remove();
+}
 //функция добавления карточек из массива и ее вызов для всех его элементов
 function renderItem (item) {
     const newCard = cardTemplate.cloneNode(true); //клонировал карточку
-    newCard.querySelector('.element__text').innerText = item.name; //добавил название места
+    newCard.querySelector('.element__text').textContent = item.name; //добавил название места
     const cardImage = newCard.querySelector('.element__image');
     cardImage.src = item.link; //добавил линк картинки
+    cardImage.alt = item.link; //добавил alt
     //добавление лайка по обработчику клика
-    newCard.querySelector('.element__like').addEventListener('click', function (evt) {
-      evt.target.classList.toggle('element__like_active');
-    });
+    newCard.querySelector('.element__like').addEventListener('click', addLike);
     //удаление карточки по обработчику клика
-    newCard.querySelector('.element__delete').addEventListener('click', function (evt) {
-      evt.target.closest('.element').remove();
-    });
+    newCard.querySelector('.element__delete').addEventListener('click', deleteCard);
     //открытие попапа с увеличенной картинкой по клику
     cardImage.addEventListener('click', function () {
       addPopupPic (item);
     });
     //добавление в начало списка
-    listElements.prepend(newCard);
+    addInBegin (newCard);
 };
 initialCards.forEach(renderItem); //добавил карточки по данным из массива
 
 //функция создания карточки, после неё обработчик на сабмит для добавления
 function createItem (evt) {
     evt.preventDefault();
-    //объеденил в массив данные названия места и ссылки, чтобы повторно использовать функцию renderItem
-    const inputArray = {
+    //объеденил данные названия места и ссылки, чтобы повторно использовать функцию renderItem
+    const inputsValue = {
       name: inputPlace.value,
       link: inputUrl.value
     };
-    renderItem (inputArray);
-    addCardClose ();
+    renderItem (inputsValue);
+    closePopup(popupCard);
+    formElementCard.reset();
 };
 formElementCard.addEventListener('submit', createItem);
